aac4c110a37dc200f8842bb4ef301982ab93df45
alex h <mralexh123@gmail.com>
2021-09-11 12:42:41 -0400

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -1,12 +1,11 @@
 exports.run = async (client, message, args, level) => {
   message.reply(`Bonks ${args} go to horny jail! <a:uwubonk:837991643557658624>`);
-  try {
-		user = await message.guild.member(message.mentions.users.first()) || await message.guild.members.fetch(target).catch(() => console.log('Error'))
-		if (message.author.id == '102131189187358720') user.voice.setChannel(message.guild.channels.cache.find(id => id.id === '841058244075192330'));
+		user = await message.guild.member(message.mentions.users.first()) || await message.guild.members.fetch(target).catch(() => client.logger.error(`could not find user.`))
 		if (user.id === '667339014625558540') return; // noodles
 		if (user.id === '102131189187358720') return; // mine
+		try {
   		user.voice.setChannel(message.guild.channels.cache.find(id => id.id === '841058244075192330'))
-		} catch (error) {
+		} catch(){
 			client.logger.error(`user ${args} was not in a voice channel.`)
 			return;
 		}
