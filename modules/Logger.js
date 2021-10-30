39fd0be65c9e8194ecc6cb919a84405771512b23
alex h <mralexh123@gmail.com>
2021-10-30 13:43:11 -0400

    added file logging for debug

--- modules/Logger.js
+++ modules/Logger.js
@@ -3,17 +3,23 @@ Logger class for easy and aesthetically pleasing console logging
 */
 const { cyan, red, magenta, gray, yellow, white, green } = require("colorette");
 const { Timestamp } = require("@sapphire/time-utilities");
+const log4js = require("log4js");
+log4js.configure({
+  appenders: { logs: { type: "file", filename: "cocktailbot.log" } },
+  categories: { default: { appenders: ["logs"], level: "log" } }
+});
+const filelogger = log4js.getLogger("logs");
 
 exports.log = (content, type = "log") => {
   const timestamp = `[${cyan(new Timestamp("YYYY-MM-DD HH:mm:ss"))}]:`;
   
   switch (type) {
-    case "log": return console.log(`${timestamp} ${gray(type.toUpperCase())} ${content} `);
-    case "warn": return console.log(`${timestamp} ${yellow(type.toUpperCase())} ${content} `);
-    case "error": return console.log(`${timestamp} ${red(type.toUpperCase())} ${content} `);
-    case "debug": return console.log(`${timestamp} ${magenta(type.toUpperCase())} ${content} `);
-    case "cmd": return console.log(`${timestamp} ${white(type.toUpperCase())} ${content}`);
-    case "ready": return console.log(`${timestamp} ${green(type.toUpperCase())} ${content}`);
+    case "log": return console.log(`${timestamp} ${gray(type.toUpperCase())} ${content} `); filelogger.info(content);
+    case "warn": return console.log(`${timestamp} ${yellow(type.toUpperCase())} ${content} `); filelogger.warn(content);
+    case "error": return console.log(`${timestamp} ${red(type.toUpperCase())} ${content} `); filelogger.error(content);
+    case "debug": return console.log(`${timestamp} ${magenta(type.toUpperCase())} ${content} `); filelogger.debug(content);
+    case "cmd": return console.log(`${timestamp} ${white(type.toUpperCase())} ${content}`); filelogger.info(content);
+    case "ready": return console.log(`${timestamp} ${green(type.toUpperCase())} ${content}`); filelogger.info(content);
     default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
   }
 }; 
