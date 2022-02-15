1717ae8e5d9ab5e64a8b1f135303f0e58c0dc55e
alex <mralexh123@gmail.com>
2022-02-15 19:34:03 +0000

    uwu 	new file:   commands/Fun/owo.js 	modified:   package-lock.json 	modified:   package.json

---
+++ commands/Fun/owo.js
@@ -0,0 +1,26 @@
+const owoify = require('owoify-js').default
+
+exports.run = async (client, message, args, level) => {
+    let msg = args.join(" ");
+    // OWOifier
+    owo = owoify(msg);
+    // Send the message
+    message.reply(owo);
+
+
+    
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["uwu"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "owo",
+  category: "Fun",
+  description: "replys with owo text",
+  usage: "owo <text>"
+};
